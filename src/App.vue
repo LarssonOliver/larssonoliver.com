3963acd129962a31561424146ea0ad47f057395c
Oliver Larsson <olli.larsson@hotmail.com>
2021-06-29 23:03:36 +0200

    Some shader work.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <home-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import HomeView from "./views/HomeView.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    HomeView,
   },
 });
 </script>
 
 <style>
+html {
+  overflow: hidden;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <space-title-componenta />
+</template>
+
+<script lang="ts">
+import SpaceTitleComponenta from "@/components/SpaceTitleComponent.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { SpaceTitleComponenta },
+});
+</script>
