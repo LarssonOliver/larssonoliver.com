d8d9ee9ba6c60b1db1ec2d143c658ba44092cd06
Oliver Larsson <olli.larsson@hotmail.com>
2021-07-14 20:29:25 +0200

    Started work on post list

---
+++ src/components/PostListComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <post-component />
+    <div v-for="i in 5" :key="i" class="item">Test</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PostComponent from "./PostComponent.vue";
+
+export default defineComponent({
+  components: { PostComponent },
+});
+</script>
+
+<style scoped>
+.contain PostComponenter {
+  width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+@media screen and (max-width: 850px) {
+  .container {
+    width: 100%;
+  }
+}
+
+.item {
+  border: 2px dotted red;
+  color: white;
+  height: 200px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <space-title-component />
+  <post-list-component />
 </template>
 
 <script lang="ts">
+import PostListComponent from "@/components/PostListComponent.vue";
 import SpaceTitleComponent from "@/components/SpaceTitleComponent.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  components: { SpaceTitleComponent },
+  components: { SpaceTitleComponent, PostListComponent },
 });
 </script>
