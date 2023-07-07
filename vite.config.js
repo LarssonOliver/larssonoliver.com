c2a2f84564dd4cada9d1a93b1cfaca5a03ae3c44
Oliver Larsson <larsson.e.oliver@gmail.com>
2023-07-07 08:47:18 +0200

    Many package upgrades

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-/* eslint-disable no-undef */
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
